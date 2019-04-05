1223f5bed6cbb819d98dc3b7655249d2f92f9400
happy_hunter <gherciu553@gmail.com>
2019-04-05 15:20:29 +0300

    removed inline svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import MainLayout from "layouts/MainLayout";
 import React from "react";
-import ReactIcon from "../static/icon.svg";
 
 const App = () => {
     return (
         <MainLayout>
             <h1>Main Page</h1>
-            <ReactIcon />
         </MainLayout>
     );
 };
