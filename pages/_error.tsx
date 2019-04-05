717da077a4c5a2f0a4556975afcaa0f867cd8a22
happy_hunter <gherciu553@gmail.com>
2019-04-05 22:52:56 +0300

    added error page

---
+++ pages/_error.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+interface IErrorProps {
+    statusCode: any;
+}
+class Error extends React.Component<IErrorProps, {}> {
+    static getInitialProps({ res, err }: { res: any; err: any }) {
+        const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+        return { statusCode };
+    }
+
+    render() {
+        const { statusCode } = this.props;
+        return <p>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</p>;
+    }
+}
+
+export default Error;
