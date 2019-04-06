431613e12a80238772b2b5cbb9e277fbc47f0b6f
happy_hunter <gherciu553@gmail.com>
2019-04-06 23:43:17 +0300

    added normalize css to root layout

---
+++ components/layouts/RootLayout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import React from 'react';
+import scss from './RootLayout.scss';
+
+interface IRootLayoutProps {
+    children: any;
+}
+const RootLayout: React.FC<IRootLayoutProps> = ({ children }: IRootLayoutProps) => (
+    <div className={scss.RootLayout}>
+        <Head>
+            <title>nextjs+typescript</title>
+            <link rel='shortcut icon' href='/static/favicon.ico' type='image/x-icon' />
+        </Head>
+        {children}
+    </div>
+);
+RootLayout.propTypes = {
+    children: PropTypes.any,
+};
+export default RootLayout;
