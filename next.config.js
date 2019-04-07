83b7b410a066edbaf975b640f10f4217b4b6c570
happy_hunter <gherciu553@gmail.com>
2019-04-08 00:57:31 +0300

    changed env structure

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withImages = require('next-images');
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = withSASS(
     withImages({
-        assetPrefix: isProd ? process.env.CDN : '',
+        assetPrefix: isProd ? process.env.ASSET_PREFIX : '',
         webpack: config => {
             config.resolve.alias.components = path.join(__dirname, 'components');
             config.resolve.alias.store = path.join(__dirname, 'store');
