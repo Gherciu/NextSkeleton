d9d0a850ffbe4ee1d6a9ba8a6b1998832f832b28
happy_hunter <gherciu553@gmail.com>
2019-04-10 23:57:14 +0300

    few fixes

--- store/state.js
+++
@@ -1,9 +0,0 @@
-import isServer from 'lib/isServer';
-
-const defaultState = {
-    count: 0,
-};
-const stateFromServer = !isServer && window.localStorage.getItem('__REDUX_STATE__');
-const initialState = isServer ? defaultState : JSON.parse(stateFromServer);
-
-export default initialState;
