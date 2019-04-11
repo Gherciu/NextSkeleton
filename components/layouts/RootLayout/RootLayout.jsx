af68a7726be317749c14136130c4fb6c16fc9c8d
happy_hunter <gherciu553@gmail.com>
2019-04-11 15:57:26 +0300

    updated favicon to png

--- components/layouts/RootLayout/RootLayout.jsx
+++ components/layouts/RootLayout/RootLayout.jsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import React from 'react';
+import favicon from 'static/favicon.png';
 import './RootLayout.scss';
-import favicon from 'static/favicon.ico';
+
 
 const RootLayout = ({ children }) => (
     <div className='root-layout'>
         <Head>
+            <link rel='icon' type='image/png' href={favicon} />
             <title>{process.env.APP_NAME}</title>
-            <link rel='shortcut icon' href={favicon} type='image/x-icon' />
         </Head>
         {children}
     </div>
--- next.config.js
+++ next.config.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
 const path = require('path');
+const withPlugins = require('next-compose-plugins');
 const withSASS = require('@zeit/next-sass');
-const withImages = require('next-images');
+const withOptimizedImages = require('next-optimized-images');
 const Dotenv = require('dotenv-webpack');
 const isProd = require('./lib/isProd');
 
-module.exports = withSASS(
-    withImages({
-        assetPrefix: isProd ? process.env.ASSET_PREFIX : '',
-        webpack: config => {
-            config.resolve.alias.components = path.join(__dirname, 'components');
-            config.resolve.alias.store = path.join(__dirname, 'store');
-            config.resolve.alias.lib = path.join(__dirname, 'lib');
-            config.resolve.alias.static = path.join(__dirname, 'static');
+module.exports = withPlugins([
+    [withSASS],
+    [withOptimizedImages, {
+        optimizeImages: isProd,
+    }],
+],
+{
+    assetPrefix: isProd ? process.env.ASSET_PREFIX : '',
+    webpack: config => {
+        config.resolve.alias.components = path.join(__dirname, 'components');
+        config.resolve.alias.store = path.join(__dirname, 'store');
+        config.resolve.alias.lib = path.join(__dirname, 'lib');
+        config.resolve.alias.static = path.join(__dirname, 'static');
 
-            config.plugins = config.plugins || [];
-            config.plugins = [
-                ...config.plugins,
-                new Dotenv({
-                    path: path.join(__dirname, '.env'),
-                    systemvars: true,
-                }),
-            ];
-            return config;
-        },
-    })
-);
+        config.plugins = config.plugins || [];
+        config.plugins = [
+            ...config.plugins,
+            new Dotenv({
+                path: path.join(__dirname, '.env'),
+                systemvars: true,
+            }),
+        ];
+        return config;
+    },
+},);
