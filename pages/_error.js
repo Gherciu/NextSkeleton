261a0f4e1e89427bc9b7f295c935d97b945e7a7f
happy_hunter <gherciu553@gmail.com>
2019-07-13 19:07:12 +0300

    updated pages

--- pages/_error.js
+++ pages/_error.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withNamespaces } from '@utils/i18n'
 
-const Error = ({ statusCode, t }) => (
-  <p>
-    {statusCode
-      ? t('common-error-with-status', { statusCode })
-      : t('common-error-without-status')}
-  </p>
-)
+const Error = ({ statusCode }) => <p>{statusCode}</p>
+
 Error.getInitialProps = async ({ res, err }) => {
   const statusCode = res ? res.statusCode : err && err.statusCode
-  return { statusCode, namespacesRequired: ['common'] }
+  return { statusCode }
 }
 Error.propTypes = {
   statusCode: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  t: PropTypes.func.isRequired,
 }
 Error.defaultProps = {
   statusCode: null,
 }
-export default withNamespaces('common')(Error)
+export default Error
