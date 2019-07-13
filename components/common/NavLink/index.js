6413d426c22863e0464081e1698440fa9fe1d090
happy_hunter <gherciu553@gmail.com>
2019-07-13 22:18:53 +0300

    updated NavLink component

--- components/common/NavLink/index.js
+++ components/common/NavLink/index.js
@@ -1,12 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { Link } from '@utils/i18n'
 import PropTypes from 'prop-types'
 
-const NavLink = ({ children, href, ...restProps }) => (
-  <Link href={href}>
-    <a href={href} {...restProps}>
-      {children}
-    </a>
+const NavLink = ({ children, href, as, scroll, ...restProps }) => (
+  <Link href={href} as={as} scroll={scroll} {...restProps}>
+    <a>{children}</a>
   </Link>
 )
 
@@ -16,7 +15,14 @@ NavLink.propTypes = {
     PropTypes.element,
     PropTypes.node,
   ]).isRequired,
-  href: PropTypes.string.isRequired,
+  href: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+  as: PropTypes.string,
+  scroll: PropTypes.bool,
+}
+
+NavLink.defaultProps = {
+  as: null,
+  scroll: true,
 }
 
 export default NavLink
